0042ea2d2cbb0716be86d0ffb3cb6cb30d1ca2f3
Sam LoCascio <sflocascio@gmail.com>
2019-11-09 20:01:10 -0500

    hotfix add fixie url to site

--- troywebsite/settings.py
+++ troywebsite/settings.py
@@ -14,6 +14,12 @@ import os
 import django_heroku
 from decouple import config
 
+proxyDict = {
+              "http"  : os.environ.get('FIXIE_URL', ''),
+              "https" : os.environ.get('FIXIE_URL', '')
+            }
+r = requests.get('http://fixie:afTQnQH6JPLiIdg@velodrome.usefixie.com:80', proxies=proxyDict)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
